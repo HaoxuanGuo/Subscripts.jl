f00bc3596bafa1a627ec2ad5f80ee527139d1cde
Haoxuan Guo <kuohaoxuan@outlook.com>
2020-12-17 22:17:18 +0800

    feat(*): functional

--- src/Subscripts.jl
+++ src/Subscripts.jl
@@ -1,5 +1,40 @@
 module Subscripts
 
-greet() = print("Hello World!")
+export super, sub
+
+super_chars = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
+  sub_chars = ['₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉']
+
+function super(s)
+    res = ""
+    for c in s
+        if c >= '0' && c <= '9'
+            res *= super_chars[c - '0' + 1]
+        end
+    end
+    return res
+end
+
+function sub(s)
+    res = ""
+    for c in s
+        if c >= '0' && c <= '9'
+            res *= sub_chars[c - '0' + 1]
+        end
+    end
+    return res
+end
+
+function recover(s::String)
+    res = ""
+    for c in s
+        for i in 1:length(super_chars)
+            if c == super_chars[i] || c == sub_chars[i]
+                res *= '0' + i - 1
+            end
+        end
+    end
+    return res
+end
 
 end # module
